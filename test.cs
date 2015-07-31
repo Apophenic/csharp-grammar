bc344f9021b6824496a154a22172ed9d8eda734a
Apophenic <jdayer9@gmail.com>
2015-07-30 20:22:21 -0400

    Include Var Declaration Rules Begun working on including rules for matching basic types

---
+++ test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+
+namespace space
+{
+    class clazz
+    {
+        public static void method(int[] args)
+        {
+            int test = 5;
+            string text = "testtext";
+            bool boolean = true;
+            byte b = 0x04;
+        }
+    }
+}
